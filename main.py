0d1cce1601c69e36512a931f483c9e39369873a5
ben h <benhughes417@gmail.com>
2024-10-10 13:17:35 -0400

    open and closes webcam

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-print("Hello, World!")
+import cv2
+import numpy as py
 
-print("Hi shiv")
+#sets video cam to default cam -- can be changed
+video = cv2.VideoCapture(0)
+
+while True:
+    #fetch webcam video
+    success, img = video.read()
+    image = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    #show webcam
+    cv2.imshow("webcam", img)
+
+    #stop program when i press q
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+#once loop is broken stops video and closes window
+video.release()
+cv2.destroyAllWindows()
